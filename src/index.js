1bd5c7f521e65928b530f5b3ede8c744315e067c
Vlad Bezsonov <vd115313@gmail.com>
2024-08-11 12:25:01 +0200

    feat: resolved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const result = [];
+  const openToClose = new Map();
+  const closeToOpen = new Map();
+
+  for (const [open, close] of bracketsConfig) {
+    openToClose.set(open, close);
+    closeToOpen.set(close, open);
+  }
+  for (const char of str) {
+    if (openToClose.has(char)) {
+      if (char === openToClose.get(char) && result.length > 0 && result[result.length - 1] === char) {
+        result.pop();
+      } else {
+        result.push(char);
+      }
+    } else if (closeToOpen.has(char)) {
+      if (result.length === 0 || result[result.length - 1] !== closeToOpen.get(char)) {
+        return false;
+      }
+      result.pop();
+    }
+  }
+
+  return result.length === 0;
+};
+
+
+
+
+// console.log(check('|()|(||)||', [['(', ')'], ['|', '|']]));
+
